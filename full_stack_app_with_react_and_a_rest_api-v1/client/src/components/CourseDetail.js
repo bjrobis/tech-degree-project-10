abcd5aaf8d843262526aef58c09c031bf0fa39b9
Brandon Robison <bjrobis@gmail.com>
2023-01-18 10:20:18 -0700

    updated courses and course files

--- full_stack_app_with_react_and_a_rest_api-v1/client/src/components/CourseDetail.js
+++ full_stack_app_with_react_and_a_rest_api-v1/client/src/components/CourseDetail.js
@@ -1,54 +1,52 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-// const Courses = (props) => {
-//     let course = props.course;    
-//     let materials = course.materialsNeeded;
-//     let materialsList;
+const Courses = (props) => {
+    let course = props.course;    
+    let materials = course.materialsNeeded;
+    let materialsList;
 
-//     materialsList = materials.map(material =>
-//         <li>{material}</li>
-//     );
+    materialsList = materials.map(material =>
+        <li>{material}</li>
+    );
 
-    
-    
-//     return(
-//         <span> </span>
-//         <div className="actions--bar">
-//             <div className="wrap">
-//                 <Link className="button" to="update-course.html">Update Course</Link>
-//                 <Link className="button" to="#">Delete Course</Link>
-//                 <Link className="button button-secondary" to="index.html">Return to List</Link>
-//             </div>
-//         </div>
-       
-//         <div className="wrap">
-//             <h2>Course Detail</h2>
-//             <form>
-//                 <div className="main--flex">
-//                     <div>
-//                         <h3 className="course--detail--title">Course</h3>
-//                         <h4 className="course--name">{course.title}</h4>
-//                         <p>{course.userId}</p>
+   return(
+    <React.Fragment>
+    <div className="actions--bar">
+        <div className="wrap">
+            <Link className="button" to="update-course.html">Update Course</Link>
+            <Link className="button" to="#">Delete Course</Link>
+            <Link className="button button-secondary" to="index.html">Return to List</Link>
+        </div>
+    </div>
 
-//                         <p>{course.description} </p>
-                                    
-//                     </div>
-                    
-//                     <div>
-//                         <h3 className="course--detail--title">Estimated Time</h3>
-//                         <p>{course.estimatedTime}</p>
+    <div className="wrap">
+        <h2>Course Detail</h2>
+        <form>
+            <div className="main--flex">
+                <div>
+                    <h3 className="course--detail--title">Course</h3>
+                    <h4 className="course--name">{course.title}</h4>
+                    <p>{course.userId}</p>
 
-//                         <h3 className="course--detail--title">Materials Needed</h3>
-//                         <ul className="course--detail--list">
-//                             {materialsList}
-//                         </ul>
-//                     </div>
-//                 </div>
-//             </form>
-//         </div>
+                    <p>{course.description} </p>
+                        
+                </div>
         
+                <div>
+                    <h3 className="course--detail--title">Estimated Time</h3>
+                    <p>{course.estimatedTime}</p>
 
-//     );
-// };
-// export default Courses;
+                    <h3 className="course--detail--title">Materials Needed</h3>
+                    <ul className="course--detail--list">
+                        {materialsList}
+                    </ul>
+                </div>
+            </div>
+        </form>
+    </div>
+    </React.Fragment>
+    
+    );
+};
+export default Courses;
--- full_stack_app_with_react_and_a_rest_api-v1/client/src/components/Courses.js
+++ full_stack_app_with_react_and_a_rest_api-v1/client/src/components/Courses.js
@@ -6,16 +6,24 @@ const Courses = (props) => {
     let courses;
 
     courses = results.map(course =>
-        <Link className="course--module course--link" to="/">
-            <h2 className="course--label">{course.title}</h2>
-            <h3 className="course--title">{course.description}</h3>
+        {
+            let id = course.id;
+            let url = `/courses/${id}`
+            let title = course.title;
+            let description = course.description;
+        <Link className="course--module course--link" to={url}>
+            <h2 className="course--label">{title}</h2>
+            <h3 className="course--title">{description}</h3>
         </Link>
+        }
     );
     
     
     
     return(
         <div className="wrap main--grid">
+            
+            {courses}
                 
             <Link className="course--module course--add--module" to="create-course.html">
                 <span class="course--add--title">
